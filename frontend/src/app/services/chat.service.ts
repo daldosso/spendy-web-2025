57363fb1b9a4988eda69f27ea944186021da8cca
Alberto Dal Dosso <alberto.daldosso@lbaconsulting.eu>
2025-02-04 15:41:15 +0100

    Chat service refactoring

--- frontend/src/app/services/chat.service.ts
+++ frontend/src/app/services/chat.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { EnvironmentService } from '../environment.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ChatService {
-  private apiUrl = 'http://localhost:3000/api/chat';
+  private apiUrl = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private environmentService: EnvironmentService) {
+    this.apiUrl = this.environmentService.getApiUrl() + '/chat';
+  }
 
   sendMessage(message: string): Observable<{ reply: string }> {
     return this.http.post<{ reply: string }>(this.apiUrl, { message });
